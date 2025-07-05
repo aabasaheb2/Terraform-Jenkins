9d7c882fbe386114cdeb74d0c7b741e9672aad92
aabasaheb2 <aabasahebjadhav22@gmail.com>
2025-07-05 13:08:55 +0530

    Update main.tf
    creating in abhijit acc, ami from mumbai

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0d03cb826412c6b0f" #ap-south-1-mumbai
+  ami           = "ami-0f918f7e67a3323f0" #ap-south-1-mumbai
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-QAinstance"
+      Name = "TF-abhijit"
   }
 }
